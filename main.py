da5fbeefe80cf3b8f8c2a01fef1a95dd8e1ca0e4
Carlos Martinez <cmartinez1499@gmail.com>
2018-05-21 01:48:27 -0500

    adding swagger documentation

--- main.py
+++ main.py
@@ -1,30 +1,86 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flasgger import Swagger
+
 import string
 
 app = Flask(__name__)
+api = Api(app)
+
+swagger = Swagger(app)
+
 all_alpha = set(string.ascii_lowercase)
 alph_len = len(all_alpha)
 
-@app.route("/")
-def main():
-	return "init"	
+parser = reqparse.RequestParser()
+parser.add_argument('string', type=str)
 
+resource_fields = {
+    'contains-all-alphas':   fields.Boolean
+}		
 
-@app.route("/v1/contains-all-alphas/subset", methods=['GET'])
-def check():
-	str_set = {c.lower() for c in request.args['string']}
-	return jsonify({'contains-all-alphas': all_alpha <= str_set})
+class Assignment1(Resource):
+	@marshal_with(resource_fields)
+	def get(self):
+		"""Checks if string contains ever lever in alphabet
+	    This implementation checks if alpha set is a subset of string
+	    ---
+	    parameters:
+	      - name: string
+	        in: query
+	        type: string
+	        required: true
+	        schema:
+	            type: string
+	            minimum: 1
+	        default: all
+	    responses:
+	      200:
+	        description: Boolean denoting if string contains all letters of alphabet
+	        schema:
+	          $ref: '#/definitions/Palette'
+	        examples:
+	          contains-all-alphas: true
+	    """
+		args = parser.parse_args(strict=True)
+		str_set = {c.lower() for c in args['string']}
+		return {'contains-all-alphas': all_alpha <= str_set}
 
-@app.route("/v1/contains-all-alphas/loop", methods=['GET'])
-def checkLoop():
-	to_return = {'contains-all-alphas': False}
+class Assignment2(Resource):
+	@marshal_with(resource_fields)
+	def get(self):
+		"""Checks if string contains ever lever in alphabet
+	    This implementation loops through alpha to find each letter in string
+	    ---
+	    parameters:
+	      - name: string
+	        in: query
+	        type: string
+	        required: true
+	        schema:
+	            type: string
+	            minimum: 1
+	        default: all
+	    responses:
+	      200:
+	        description: Boolean denoting if string contains all letters of alphabet
+	        schema:
+	          $ref: '#/definitions/Palette'
+	        examples:
+	          contains-all-alphas: true
+	    """
+		to_return = {'contains-all-alphas': False}
+		args = parser.parse_args(strict=True)
 
-	if (len(request.args['string']) < alph_len):
+		if (len(args['string']) < alph_len):
+			return to_return
+
+		str_set = {char.lower() for char in args['string']}
+		to_return['contains-all-alphas'] = len([letter for letter in all_alpha if letter in str_set]) == alph_len
 		return to_return
 
-	str_set = {c.lower() for c in request.args['string']}
-	to_return['contains-all-alphas'] = len([a for a in all_alpha if a in str_set]) == alph_len
-	return jsonify(to_return)
+api.add_resource(Assignment1, '/v1/contains-all-alphas/subset')
+api.add_resource(Assignment2, '/v1/contains-all-alphas/loop')
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
