496ff7b1bb0fc492d6f76ed2b7ca1a1561d447d6
Carlos Martinez <cmartinez1499@gmail.com>
2018-05-20 22:47:40 -0500

    creating flask api and implementing solutions

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import string
+
 app = Flask(__name__)
+all_alpha = set(string.ascii_lowercase)
+alph_len = len(all_alpha)
 
 @app.route("/")
 def main():
-	return "init"
+	return "init"	
+
+
+@app.route("/v1/contains-all-alphas/subset", methods=['GET'])
+def check():
+	str_set = {c.lower() for c in request.args['string']}
+	return jsonify({'contains-all-alphas': all_alpha <= str_set})
+
+@app.route("/v1/contains-all-alphas/loop", methods=['GET'])
+def checkLoop():
+	to_return = {'contains-all-alphas': False}
+
+	if (len(request.args['string']) < alph_len):
+		return to_return
+
+	str_set = {c.lower() for c in request.args['string']}
+	to_return['contains-all-alphas'] = len([a for a in all_alpha if a in str_set]) == alph_len
+	return jsonify(to_return)
 
 if __name__ == '__main__':
 	app.run()
